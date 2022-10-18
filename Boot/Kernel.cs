9ce592ae5a29dbc699872572f36b318d0bf1819b
Robert Smith <master_schmadoodle@outlook.com>
2022-10-18 15:24:11 +0100

    Minor changes

--- Boot/Kernel.cs
+++ Boot/Kernel.cs
@@ -7,6 +7,7 @@ using Point = Cosmos.System.Graphics.Point;
 using Console = System.Console;
 using System.Collections.Generic;
 using Cosmos.System;
+using Boot.Systems;
 
 namespace Boot {
 	public class Kernel : Sys.Kernel {
@@ -126,21 +127,19 @@ namespace Boot {
 
 				}
 			}
-			if(!EnableUI) {
-				GetScreenInfo();
-				// To calculate screen PPI we need the Width/Heigth of the screen in inches and the Width/Height of the screen in pixels (X/Y Divided by Width/Height)
-				if (Screen.PPI == null && ((Screen.W != null && Screen.X != null) || (Screen.H != null && Screen.Y != null))) CalculateScreenPPI();
-				// To calculate the screen size we need the screen size in pixels and the screen PPI (X/Y divided by PPI)
-				if (Screen.PPI != null &&  (Screen.W != null || Screen.H != null) && (Screen.X != null && Screen.Y != null) ) CalculateScreenSize();
-				// Calculate how many cells will be on the screen
-				CalculateCells();
+			GetScreenInfo();
+			// To calculate screen PPI we need the Width/Heigth of the screen in inches and the Width/Height of the screen in pixels (X/Y Divided by Width/Height)
+			if (Screen.PPI == null && ((Screen.W != null && Screen.X != null) || (Screen.H != null && Screen.Y != null))) CalculateScreenPPI();
+			// To calculate the screen size we need the screen size in pixels and the screen PPI (X/Y divided by PPI)
+			if (Screen.PPI != null &&  (Screen.W != null || Screen.H != null) && (Screen.X != null && Screen.Y != null) ) CalculateScreenSize();
+			// Calculate how many cells will be on the screen
+			CalculateCells();
 
-				Canvas = FullScreenCanvas.GetFullScreenCanvas(new Mode(Screen.X, Screen.Y, Colors.Depth));
-				Canvas.Clear(Colors.Background);
-				Write("Welcome to the Aurora Basic Operation System Project.", Screen.Font, Colors.Primary, true);
-				WriteLine("Successfully booted.", Screen.Font, Colors.Success, true);
-				WriteLine("ABOS> ", Screen.Font, Colors.Foreground, true);
-			}
+			Canvas = FullScreenCanvas.GetFullScreenCanvas(new Mode(Screen.X, Screen.Y, Colors.Depth));
+			Canvas.Clear(Colors.Background);
+			Write("Welcome to the Aurora Basic Operation System Project.", Screen.Font, Colors.Primary, true);
+			WriteLine("Successfully booted.", Screen.Font, Colors.Success, true);
+			WriteLine("ABOS> ", Screen.Font, Colors.Foreground, true);
 		}
 
 		protected override void Run() {
@@ -167,8 +166,6 @@ namespace Boot {
 						Canvas.DrawFilledRectangle(new Pen(Colors.Background), GetCellPos(Cursor.Pos.X, Cursor.Pos.Y), Grid.Cell.Width, Grid.Cell.Height);
 					} else if (input.Key == ConsoleKey.Enter) {
 						
-					} else if (input.Key == ConsoleKey.Escape) {
-						// 
 					} else if(	Array.Exists(Keyboard.Letters, element => element == input.Key) ||
 								Array.Exists(Keyboard.Numbers, element => element == input.Key) ||
 								Array.Exists(Keyboard.Symbols, element => element == input.Key) ||
@@ -181,91 +178,5 @@ namespace Boot {
                 }
             }
         }
-        /// <summary>
-        /// Writes a string from the current cursor position
-        /// </summary>
-        private static void Write(string str, PCScreenFont font, Color color, bool system = false) {
-			for (int i = 0; i < str.Length; i++) {
-				Canvas.DrawString(str[i].ToString(), font, new Pen(color), GetCellPos(Cursor.Pos.X, Cursor.Pos.Y));
-				Subtractable++;
-				if (Cursor.Pos.X == Grid.Columns) {
-					Cursor.Pos.X = 1;
-					Cursor.Pos.Y++;
-				} else {
-					Cursor.Pos.X++;
-				}
-			}
-			if(system) {
-				Subtractable = 0;
-            }
-        }
-        /// <summary>
-		/// Writes a string from a new line 
-        /// </summary>
-        private static void WriteLine(string str, PCScreenFont font, Color color, bool system = false) {
-			Cursor.Pos.Y++;
-			Cursor.Pos.X=1;
-			Write(str, font, color, system);
-		}
-		/// <summary>
-		/// Gets the screen size and Pixels Per Inch if the information is available.
-		/// TODO: Build function to actually get the size of the screen
-		/// </summary>
-		private static void GetScreenInfo() {
-            // HD
-            //Screen.X = 1280;
-            //Screen.Y = 720;
-            //Screen.PPI = 183;
-            // FHD
-            Screen.X = 1920;
-            Screen.Y = 1080;
-            Screen.PPI = 165;
-            // 4K
-            //Screen.X = 4096;
-            //Screen.Y = 2560;
-            //Screen.PPI = 150;
-        }
-        /// <summary>
-		/// Calculates the screen size in Inches
-        /// </summary>
-        private static void CalculateScreenSize() {
-			//(X/Y divided by PPI)
-			if(Screen.X != null && Screen.Y != null && Screen.PPI != null) {
-				Screen.W = Screen.Y / Screen.PPI;
-				Screen.H = Screen.Y / Screen.PPI;
-			} else {
-				// Unable to get screen size
-			}
-        }
-        /// <summary>
-		/// Calculates the screens Pixels Per Inch
-        /// </summary>
-        private static void CalculateScreenPPI() {
-			// (X/Y Divided by Width/Height)
-			if(Screen.X != null && Screen.W != null) {
-				Screen.PPI = Screen.X / Screen.W;
-			} else if(Screen.Y != null && Screen.H != null) {
-				Screen.PPI = Screen.Y / Screen.H;
-			} else {
-				// Unable to get screen PPI
-			}
-        }
-        /// <summary>
-        /// Calculates the quantity and size of cells for the user's screen
-		/// TODO: Add Scaling functionality
-        /// </summary>
-        private static void CalculateCells() {
-			var CM = (int)Math.Floor(Screen.PPI/2.54);
-			Grid.Cell.Width = CM/8;
-			Grid.Cell.Height = CM/4;
-			Grid.Columns = Screen.X/Grid.Cell.Width;
-			Grid.Rows = Screen.Y/Grid.Cell.Height;
-        }
-        /// <summary>
-        /// Converts cell coordinates to PX coordinates
-        /// </summary>
-        private static Point GetCellPos(int CellX, int CellY) {
-			return new Point((CellX-1)* Grid.Cell.Width, (CellY-1)* Grid.Cell.Height);
-		}
 	}
 }
